9b53b19681b0ccdedccb57bfdac140e1a7e15921
PodssilDev <john.serrano@usach.cl>
2023-09-10 18:26:30 -0300

    [feat]: Lab 1 - getopt, idea secuencial, ideal paralela (falta fix)

---
+++ Laboratorio 1/dilation.c
@@ -0,0 +1,153 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <emmintrin.h>
+
+// Función para realizar la dilatación de la imagen usando SIMD
+void dilateSIMD(const unsigned char *inputImage, unsigned char *outputImage, int width, int height)
+{
+    int i, j;
+
+    for (i = 1; i < height - 1; i++)
+    {
+        for (j = 1; j < width - 1; j++)
+        {
+            // Cargar los píxeles vecinos en registros SIMD
+            printf("%d\n", i * width + 1);
+            __m128i R0 = _mm_set1_epi8(inputImage[i * width + j]);
+            __m128i R1 = _mm_set1_epi8(inputImage[(i - 1) * width + j]);
+            __m128i R2 = _mm_set1_epi8(inputImage[(i + 1) * width + j]);
+            __m128i R3 = _mm_set1_epi8(inputImage[i * width + j - 1]);
+            __m128i R4 = _mm_set1_epi8(inputImage[i * width + j + 1]);
+
+            // Aplicar la operación mm_max() para obtener el píxel resultante
+            __m128i result = _mm_max_epu8(R0, R1);
+            result = _mm_max_epu8(result, R2);
+            result = _mm_max_epu8(result, R3);
+            result = _mm_max_epu8(result, R4);
+
+            // Extraer el byte en la posición 0 del registro result
+            outputImage[i * width + j] = _mm_extract_epi16(result, 0) & 0xFF;
+        }
+    }
+}
+
+// Función para realizar la dilatación de la imagen de forma secuencial
+void dilateSequential(const unsigned char *inputImage, unsigned char *outputImage, int width, int height)
+{
+    int i, j;
+
+    for (i = 1; i < height - 1; i++)
+    {
+        for (j = 1; j < width - 1; j++)
+        {
+            unsigned char maxPixel = 0;
+
+            // Obtener el máximo de los píxeles vecinos
+            for (int m = -1; m <= 1; m++)
+            {
+                for (int n = -1; n <= 1; n++)
+                {
+                    unsigned char pixel = inputImage[(i + m) * width + j + n];
+                    if (pixel > maxPixel)
+                    {
+                        maxPixel = pixel;
+                    }
+                }
+            }
+
+            // Almacenar el resultado en la imagen de salida
+            outputImage[i * width + j] = maxPixel;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char *inputFileName = NULL;
+    char *sequentialOutputFileName = NULL;
+    char *simdOutputFileName = NULL;
+    int width = 0;
+    int opt;
+
+    // Procesar los argumentos de línea de comandos usando getopt
+    while ((opt = getopt(argc, argv, "i:s:p:N:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            inputFileName = optarg;
+            break;
+        case 's':
+            sequentialOutputFileName = optarg;
+            break;
+        case 'p':
+            simdOutputFileName = optarg;
+            break;
+        case 'N':
+            width = atoi(optarg);
+            break;
+        default:
+            fprintf(stderr, "Uso: %s -i input.pgm -s output_sequential.pgm -p output_simd.pgm -N width\n", argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (inputFileName == NULL || sequentialOutputFileName == NULL || simdOutputFileName == NULL || width <= 0)
+    {
+        fprintf(stderr, "Faltan argumentos o argumentos inválidos.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Leer la imagen de entrada desde un archivo PGM binario (formato P5)
+    FILE *inputFile = fopen(inputFileName, "rb");
+    if (inputFile == NULL)
+    {
+        perror("Error al abrir el archivo de entrada");
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned char *inputImage = (unsigned char *)malloc(width * width);
+    fread(inputImage, 1, width * width, inputFile);
+    fclose(inputFile);
+
+    // Crear una imagen de salida del mismo tamaño
+    unsigned char *sequentialOutputImage = (unsigned char *)malloc(width * width);
+    unsigned char *simdOutputImage = (unsigned char *)malloc(width * width);
+
+    // Realizar la dilatación de forma secuencial
+    dilateSequential(inputImage, sequentialOutputImage, width, width);
+
+    // Realizar la dilatación usando SIMD
+    dilateSIMD(inputImage, simdOutputImage, width, width);
+
+    // Escribir la imagen resultante de forma secuencial en un archivo PGM binario
+    FILE *sequentialOutputFile = fopen(sequentialOutputFileName, "wb");
+    if (sequentialOutputFile == NULL)
+    {
+        perror("Error al abrir el archivo de salida secuencial");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(sequentialOutputFile, "P5\n%d %d\n255\n", width, width);
+    fwrite(sequentialOutputImage, 1, width * width, sequentialOutputFile);
+    fclose(sequentialOutputFile);
+
+    // Escribir la imagen resultante usando SIMD en un archivo PGM binario
+    FILE *simdOutputFile = fopen(simdOutputFileName, "wb");
+    if (simdOutputFile == NULL)
+    {
+        perror("Error al abrir el archivo de salida SIMD");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(simdOutputFile, "P5\n%d %d\n255\n", width, width);
+    fwrite(simdOutputImage, 1, width * width, simdOutputFile);
+    fclose(simdOutputFile);
+
+    // Liberar la memoria
+    free(inputImage);
+    free(sequentialOutputImage);
+    free(simdOutputImage);
+
+    return 0;
+}
